8dbe7a89f5ab14d803892aaed57b50de33d1eee9
Denis <borbotko.den@mail.ru>
2021-09-16 13:25:45 +0300

    Resolved conflict

--- test.py
+++ test.py
@@ -1,6 +1,1 @@
-print("Python education")
-=======
-print("education")
-# new string
-print("DICT")
->>>>>>> abb4fd6... test.py added to feature_branch with change on string#3
+print("python education")
