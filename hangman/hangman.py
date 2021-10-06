c2f3a4b612b7148893e07c99dd8312fd2419a873
Denis <borbotko.den@mail.ru>
2021-10-06 16:17:38 +0300

    hangman stage 2

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,1 +1,9 @@
 print("The game will start soon")
+print("guess the word")
+word = 'python'
+print('ugadai')
+answer = str(input())
+if answer == word:
+    print("u won")
+else:
+    print ("u lose")
