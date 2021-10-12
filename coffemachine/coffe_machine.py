8ef6b0b95b0ff8ecc868052f12288ad5f3155372
Denis <borbotko.den@mail.ru>
2021-10-12 18:43:26 +0300

    coffee_machine stage 3

--- coffemachine/coffe_machine.py
+++ coffemachine/coffe_machine.py
@@ -1,5 +1,3 @@
-import random
-
 print("""Starting to make a coffee
 Grinding coffee beans
 Boiling water
@@ -7,14 +5,20 @@ Mixing boiled water with crushed coffee beans
 Pouring coffee into the cup
 Pouring some milk into the cup
 Coffee is ready!""")
-# water = int(random.randint(0, 1000))
-# milk = int(random.randint(0, 1000))
-# zerna = int(random.randint(0, 1000))
-water_cup = 200
-milk_cup = 50
-zerna_cup = 15
+
+water_cup = int(input("Write how many ml of water the coffee machine has: "))
+milk_cup = int(input("Write how many ml of milk the coffee machine has: "))
+beams_cup = int(input("Write how many grams of coffee beans the coffee machine has: "))
 want_coffee = int(input("vvedite colwo chashek: "))
-print("For ", want_coffee,  " cups you will need")
-print(str(water_cup * want_coffee))
-print(str(milk_cup * want_coffee))
-print(str(zerna_cup * want_coffee))
+
+one_cup_w = (water_cup / 250)
+one_cup_m = (milk_cup / 50)
+one_cup_b = (beams_cup / 15)
+one_cup_minimum = min(one_cup_w, one_cup_m, one_cup_b)
+one_cup_N = int(one_cup_minimum - want_coffee)
+one_cup_Nmin1 = int(want_coffee - one_cup_minimum)
+
+if one_cup_minimum >= want_coffee:
+    print("Yes, I can make that amount of coffee and " + str(one_cup_N) + " even more")
+elif one_cup_minimum < want_coffee:
+    print("No, I can make only " + str(one_cup_Nmin1) + " cups of coffee")
