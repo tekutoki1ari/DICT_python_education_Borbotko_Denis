739eafb41d68eb4e3ae7c2ceb17a805abe50a0d3
Denis <borbotko.den@mail.ru>
2021-11-02 22:39:30 +0200

    coffee_machine stage 4

--- coffemachine/coffe_machine.py
+++ coffemachine/coffe_machine.py
@@ -1,4 +1,6 @@
-print("""Starting to make a coffee
+import random
+text = ("""
+Starting to make a coffee
 Grinding coffee beans
 Boiling water
 Mixing boiled water with crushed coffee beans
@@ -6,19 +8,145 @@ Pouring coffee into the cup
 Pouring some milk into the cup
 Coffee is ready!""")
 
-water_cup = int(input("Write how many ml of water the coffee machine has: "))
-milk_cup = int(input("Write how many ml of milk the coffee machine has: "))
-beams_cup = int(input("Write how many grams of coffee beans the coffee machine has: "))
-want_coffee = int(input("vvedite colwo chashek: "))
+waters = random.randint(0, 1000)
+milks = random.randint(0, 500)
+beamss = random.randint(0, 250)
+cupss = random.randint(0, 25)
 
-one_cup_w = (water_cup / 250)
-one_cup_m = (milk_cup / 50)
-one_cup_b = (beams_cup / 15)
-one_cup_minimum = min(one_cup_w, one_cup_m, one_cup_b)
-one_cup_N = int(one_cup_minimum - want_coffee)
-one_cup_Nmin1 = int(want_coffee - one_cup_minimum)
+cost_espresso = 4
+cost_latte = 7
+cost_cappuchino = 6
 
-if one_cup_minimum >= want_coffee:
-    print("Yes, I can make that amount of coffee and " + str(one_cup_N) + " even more")
-elif one_cup_minimum < want_coffee:
-    print("No, I can make only " + str(one_cup_Nmin1) + " cups of coffee")
+waterf = int(0)
+milkf = int(0)
+beamsf = int(0)
+cupsf = int(0)
+n = 0
+
+def espresso(message_espresso):
+    global water, milk, beams, cups, cost_espresso, waterf, milkf, beamsf, cupsf, n
+    water = int((waters + waterf) / 250)
+    beams = int((beamss + beamsf) / 16)
+    cups = int(cupss + cupsf)
+    minimum = min([water, beams, cups])
+    cup = int(input("How many cups of coffe u need?"))
+    final_cost = cup * cost_espresso
+    if minimum >= cup:
+        money = int(input(f"Please, insert cash - {final_cost}"))
+        if money == final_cost:
+            print(text)
+            n = n + money
+            coffemachine('')
+        elif money < final_cost:
+            print("You have insert not enough amount of money")
+            espresso('espresso')
+        else:
+            change = money - final_cost
+            print("Take ur change")
+            print(text)
+    elif (minimum < cup):
+        print("We have not enough ingredients, sorry")
+        print("pls, fill machine")
+        coffemachine('')
+
+
+def latte(message_latte):
+    global water, milk, beams, cups, cost_latte, waterf, milkf, beamsf, cupsf, n
+    water =  int((waters + waterf) / 350)
+    milk = int((milks + milkf) / 75)
+    beams = int((beamss - beamsf) / 20)
+    cups = int(cupss + cupsf)
+    minimum = min([water, milk, beams, cups])
+    cup = int(input("How many cups of coffe u need?"))
+    final_cost = cup * cost_latte
+    if (minimum >= cup):
+        money = int(input(f"Please, insert cash - {final_cost}"))
+        if money == final_cost:
+            print(text)
+            n = n + money
+        elif money < final_cost:
+            print("You have insert not enough amount of money")
+            latte('latte')
+        else:
+            change = money - final_cost
+            print(f"Take ur change {change}")
+            print(text)
+    elif (minimum < cup):
+        print("We have not enough ingredients, sorry")
+        print("pls, fill machine")
+        coffemachine('')
+
+
+def cappuchino(message_latte):
+    global water, milk, beams, cups, cost_cappuchino, waterf, milkf, beamsf, cupsf, n
+    water = int((waters + waterf) / 200)
+    milk = int((milks + milkf) / 100)
+    beams = int((beamss - beamsf) / 12)
+    cups = int(cupss + cupsf)
+    minimum = min([water, milk, beams, cups])
+    cup = int(input("How many cups of coffe u need?"))
+    final_cost = cup * cost_cappuchino
+
+    if (minimum >= cup):
+        money = int(input(f"Please, insert cash - {final_cost}"))
+        if money == final_cost:
+            print(text)
+            n = n + money
+        elif money < final_cost:
+            print("You have insert not enough amount of money")
+            cappuchino('cappuchino')
+        else:
+            change = money - final_cost
+            print(f"Take ur change {change}")
+            print(text)
+    elif (minimum < cup):
+        print("We have not enough ingredients, sorry")
+        print("pls, fill machine")
+        coffemachine('')
+
+def take():
+    global n
+    print(f"{n} money")
+    n = 0
+    coffemachine('')
+
+
+def coffemachine(message_coffemachine):
+    global waters, waterf, milks, milkf, beamss, beamsf, cupss, cupsf, cost_latte, cost_espresso, cost_cappuchino, n
+    print("""What u whant to do?: 
+    1 - buy, 
+    2 - fill, 
+    3 - take, 
+    4 - show amount of ingredients""")
+    answer = int(input())
+    if answer == 1:
+        print("""What coffee do u want?
+        1 - espresso
+        2 - latte
+        3 - cappuccino""")
+        choose_coffee = int(input())
+        if choose_coffee == 1:
+            espresso('')
+        elif choose_coffee == 2:
+            latte('')
+        elif choose_coffee == 3:
+            cappuchino('')
+    elif answer == 2:
+        waterf = int(input("insert amount of added water"))
+        milkf = int(input("insert amount of added milk"))
+        beamsf = int(input("insert amount of added beams"))
+        cupsf = int(input("insert amount of added cups"))
+    elif answer == 3:
+        take()
+
+    elif answer == 4:
+        print(waters + waterf)
+        print(milks + milkf)
+        print(beamss + beamsf)
+        print(cupss + cupsf)
+        print(n)
+    coffemachine('')
+
+
+coffemachine('')
+# сделай 4 коммит, это все 4 коммит, 5 в будущем
