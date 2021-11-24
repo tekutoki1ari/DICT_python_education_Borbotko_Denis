999bba1e4815d3bbc5ec708c8f2cb4585f65092a
Denis <borbotko.den@mail.ru>
2021-11-24 20:10:44 +0200

    creditcalculator stage 3

--- CreditCalculator/creditcalculator.py
+++ CreditCalculator/creditcalculator.py
@@ -1,33 +1,55 @@
-creditsum = int(input("pls, enter the summ of ur credit "))
+import math
 
 
-def monthly_payment():
-    global creditsum
-    months = int(input("pls, enter amount of months "))
-    payment = creditsum / months
-    lastpayment = creditsum - (months - 1) * payment
-    if payment == lastpayment:
-        print(f"ur monthly payment is {round(payment)}")
-    elif payment != lastpayment:
-        print(f"ur monthly payment is {round(payment)} and the lastone is {lastpayment}")
+def annual_payment():
+    p = float(input("pls, enter summ of the credit "))
+    i = float(input("pls, enter the interest rate "))
+    n = float(input("pls, enter the amount of payments "))
+    ii = i / (12 * 100)
+    annualpayment = p * (ii * math.pow(1 + ii, n) / (math.pow(1 + ii, n) - 1))
+    annul = math.ceil(annualpayment)
+    print(f"your monthly payment = {annul}!")
 
-def last_payment():
-    global creditsum
-    monthlypayment = int(input("pls, enter the monthly payment "))
-    amount_months = creditsum / monthlypayment
-    print(f"it will take {round(amount_months)} months to repay the loan")
+
+def loan_principle():
+    a = float(input("pls, enter the annual payment "))
+    i = float(input("pls, enter the interest rate "))
+    n = float(input("pls, enter the amount of payments "))
+    ii = i / (12 * 100)
+    loanprinciple = a / ((ii * math.pow(1 + ii, n)) / (math.pow(1 + ii, n) - 1))
+    loanprin = math.ceil(loanprinciple)
+    print(f"your loan principle = {loanprin}!")
+
+
+def number_of_payments():
+    p = float(input("pls, enter summ of the credit "))
+    a = float(input("pls, enter the annunity monthly payment "))
+    i = float(input("pls, enter the loan interest rate "))
+    ii = i / (12 * 100)
+    n = math.log(a / (a - (ii * p)), 1 + ii)
+    months = math.ceil(n % 12)
+    years = math.floor(n / 12)
+
+    if n < 12:
+        print(f"it will takes {months} months")
+    elif n > 12:
+        print(f"it will takes {years} years and {months} months")
 
 
 def start():
-    Start = int(input("""what u want to calculate
-1 - number of monthly payments
-2 - for the monthly payment"""))
-    if Start == 1:
-        last_payment()
-    elif Start == 2:
-        monthly_payment()
+    sstart = int(input("""What do you want to calculate?
+type "1" for number of monthly payments,
+type "2" for annuity monthly payment amount,
+type "3" for loan principal: """))
+    if sstart == 1:
+        number_of_payments()
+    elif sstart == 2:
+        annual_payment()
+    elif sstart == 3:
+        loan_principle()
     else:
         print("try again")
         start()
+
 
 start()
