431f071f8d7dd7bb90869699b017e1ff12624ca0
Denis <borbotko.den@mail.ru>
2021-11-26 02:00:35 +0200

    creditcalculator final stage fixed

--- CreditCalculator/creditcalculator.py
+++ CreditCalculator/creditcalculator.py
@@ -1,39 +1,47 @@
 import math
 
 
-def annual_payment():
-    p = float(input("pls, enter summ of the credit "))
-    i = float(input("pls, enter the interest rate "))
-    n = float(input("pls, enter the amount of payments "))
-    ii = i / (12 * 100)
-    annualpayment = p * (ii * math.pow(1 + ii, n) / (math.pow(1 + ii, n) - 1))
-    annul = math.ceil(annualpayment)
-    print(f"your monthly payment = {annul}!")
+def annual_payment_func():
+    principal_credit_sum = float(input("pls, enter sum of the credit "))
+    interest_rate = float(input("pls, enter the interest rate "))
+    amount_of_payments = float(input("pls, enter the amount of payments "))
+    nominal_rate_percents = interest_rate / (12 * 100)
+    annual_payment = principal_credit_sum * (nominal_rate_percents *
+                                             math.pow(1 + nominal_rate_percents, amount_of_payments)
+                                             / (math.pow(1 + nominal_rate_percents, amount_of_payments) - 1))
+    annual_ceil = math.ceil(annual_payment)
+    print(f"your monthly payment = {annual_ceil}!")
+    start()
 
 
-def loan_principle():
-    a = float(input("pls, enter the annual payment "))
-    i = float(input("pls, enter the interest rate "))
-    n = float(input("pls, enter the amount of payments "))
-    ii = i / (12 * 100)
-    loanprinciple = a / ((ii * math.pow(1 + ii, n)) / (math.pow(1 + ii, n) - 1))
-    loanprin = math.ceil(loanprinciple)
-    print(f"your loan principle = {loanprin}!")
+def loan_principle_func():
+    annual_payment = float(input("pls, enter the annual payment "))
+    interest_rate = float(input("pls, enter the interest rate "))
+    nominal_rate = float(input("pls, enter the amount of payments "))
+    interest_rate_percents = interest_rate / (12 * 100)
+    loan_principle = annual_payment / ((interest_rate_percents * math.pow(1 + interest_rate_percents, nominal_rate))
+                                       / (math.pow(1 + interest_rate_percents, nominal_rate) - 1))
+    loan_principle_ceil = math.ceil(loan_principle)
+    print(f"your loan principle = {loan_principle_ceil}!")
+    start()
 
 
 def number_of_payments():
-    p = float(input("pls, enter summ of the credit "))
-    a = float(input("pls, enter the annunity monthly payment "))
-    i = float(input("pls, enter the loan interest rate "))
-    ii = i / (12 * 100)
-    n = math.log(a / (a - (ii * p)), 1 + ii)
-    months = math.ceil(n % 12)
-    years = math.floor(n / 12)
+    principle_credit_sum = float(input("pls, enter sum of the credit "))
+    annual_payment = float(input("pls, enter the annual monthly payment "))
+    interest_rate = float(input("pls, enter the loan interest rate "))
+    interest_rate_percents = interest_rate / (12 * 100)
+    nominal_rate = math.log(annual_payment /
+                            (annual_payment - (interest_rate_percents * principle_credit_sum)),
+                            1 + interest_rate_percents)
+    months = math.ceil(nominal_rate % 12)
+    years = math.floor(nominal_rate / 12)
 
-    if n < 12:
+    if nominal_rate < 12:
         print(f"it will takes {months} months")
-    elif n > 12:
+    elif nominal_rate > 12:
         print(f"it will takes {years} years and {months} months")
+    start()
 
 
 def diff():
@@ -48,20 +56,21 @@ def diff():
         overpayment = overpayment + differ
         continue
     print(f"Overpayment = {overpayment - principal}")
+    start()
 
 
 def start():
     select = int(input("""What do you want to calculate?
-type "1" for number of monthly payments,
-type "2" for annuity monthly payment amount,
-type "3" for loan principal: 
-type "4" for diff: """))
+                        type "1" for number of monthly payments,
+                        type "2" for annuity monthly payment amount,
+                        type "3" for loan principal: 
+                        type "4" for diff: """))
     if select == 1:
         number_of_payments()
     elif select == 2:
-        annual_payment()
+        annual_payment_func()
     elif select == 3:
-        loan_principle()
+        loan_principle_func()
     elif select == 4:
         diff()
     else:
