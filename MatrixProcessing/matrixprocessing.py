c6814359d110bcaabaef4fd49c8f02c40785a428
Denis <borbotko.den@mail.ru>
2021-12-06 21:06:02 +0200

    matrixprocessing stage 3

--- MatrixProcessing/matrixprocessing.py
+++ MatrixProcessing/matrixprocessing.py
@@ -1,19 +1,4 @@
-#
-#
-# def matrix1():
-#     rows = int(input("how many rows do u want to use? "))
-#     columns = int(input("how many columns do u want to use? "))
-#     matrix = [[0 for x in range(columns)] for y in range(rows)]
-#     for y in range(rows):
-#         for x in range(columns):
-#             matrix[y][x] = int(input("Enter numbers: "))
-#     print("\nThe original matrix is...")
-#     for y in range(rows):
-#         print("\n")
-#         for x in range(columns):
-#             print(format(matrix[y][x], "5d"), end="")
-#     return rows, columns, matrix
-#
+
 
 def matrix_addition():
     matrix_a = []
@@ -40,12 +25,10 @@ def matrix_addition():
         for i in range(0, len(matrix_plus), int(size[1]) + 1):
             matrix_plus.insert(i, '\n')
         print(' '.join(map(str, matrix_plus)))
+        main()
 
 
-# matrix_addition()
-
-
-def multiply_matrix():
+def matrix_constant_multiplication():
     matrix_a = []
     matrix_multiply = []
     size = input(f'Input matrix size\n>>> ')
@@ -65,6 +48,64 @@ def multiply_matrix():
         for i in range(0, len(matrix_multiply), int(size[1]) + 1):
             matrix_multiply.insert(i, '\n')
         print(' '.join(map(str, matrix_multiply)))
+        main()
 
 
-multiply_matrix()
+def matrix_multiplication():
+    matrix_a = []
+    matrix_b = []
+    size = input(f'Input matrix size\n: ')
+    size = size.replace(' ', '')
+    m = int(size[0])
+    n = int(size[1])
+    if size.isnumeric():
+        print('Matrix A')
+        for i in range(n):
+            new_list = []
+            for j in range(m):
+                number = input(f'Element {j}\n>>> ')
+                new_list.append(number)
+            matrix_a.append(new_list)
+        print('Matrix B')
+        for i in range(n):
+            new_list = []
+            for j in range(m):
+                number = input(f'Element {j}\n>>> ')
+                new_list.append(number)
+            matrix_b.append(new_list)
+        r = []
+        m = []
+        for i in range(len(matrix_a)):
+            for j in range(len(matrix_b[0])):
+                sums = 0
+                for k in range(len(matrix_b)):
+                    sums = sums + (int(matrix_a[i][k]) * int(matrix_a[k][j]))
+                r.append(sums)
+            m.append(r)
+            r = []
+        for i in m:
+            print(' '.join(map(str, i)))
+            main()
+
+
+def main():
+    choose = int(input('''select funct:
+                        1 - matrix addition
+                        2 - matrix constant multiplication
+                        3 - matrix multiplication
+                        0 - exit
+                        '''))
+    if choose == 1:
+        matrix_addition()
+    elif choose == 2:
+        matrix_constant_multiplication()
+    elif choose == 3:
+        matrix_multiplication()
+    elif choose == 0:
+        print('bye')
+    else:
+        print('try again')
+        main()
+
+
+main()
