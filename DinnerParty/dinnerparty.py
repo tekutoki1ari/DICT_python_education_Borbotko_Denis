358cf49e45986b60d545a34e4bec103898208570
Denis <borbotko.den@mail.ru>
2021-12-08 22:23:18 +0200

    dinnerparty stage 4

--- DinnerParty/dinnerparty.py
+++ DinnerParty/dinnerparty.py
@@ -2,9 +2,10 @@ import random
 
 
 class DinnerParty:
-    def __init__(self, friends, money):
+    def __init__(self, friends, money, lucky):
         self.friends = friends
         self.money = money
+        self.lucky = lucky
 
     def invite(self):
         self.friends = ['Denis', 'Marlen', 'Bogdan', 'Stanislaw']
@@ -18,7 +19,7 @@ class DinnerParty:
                             2 - yes
                             '''))
             if select == 1:
-                print('no one going to be lucky:>>> ')
+                print('no one going to be lucky')
                 cash = int(input("enter amount of money:>>> "))
                 amount = cash / invite_list_amount
                 sum_true = round(amount, 2)
@@ -26,9 +27,19 @@ class DinnerParty:
                     self.money.append(sum_true)
                 print(dict(zip(self.friends[:invite_list_amount], self.money)))
             else:
-                lucky = random.choice(self.friends)
-                print(f'the {lucky} is lucky')
+                lucky = random.choice(self.friends[:invite_list_amount])
+                print(lucky)
+                cash = int(input("enter amount of money:>>> "))
+                amount = cash / (invite_list_amount - 1)
+                sum_true = round(amount, 2)
+                clever = self.friends.index(lucky)
+                for i in range(invite_list_amount):
+                    if i == clever:
+                        self.money.append(0)
+                    else:
+                        self.money.append(sum_true)
+                print(dict(zip(self.friends[:invite_list_amount], self.money)))
 
 
-dinner_party = DinnerParty([], [])
+dinner_party = DinnerParty([], [], 'default')
 dinner_party.invite()
