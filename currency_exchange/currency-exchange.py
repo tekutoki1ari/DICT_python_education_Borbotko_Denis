d6555001ac03f59ebe0956dc6f6bec7160048507
Denis <borbotko.den@mail.ru>
2022-06-11 17:31:04 +0300

    currency-exchange st2

--- currency_exchange/currency-exchange.py
+++ currency_exchange/currency-exchange.py
@@ -1,10 +1,21 @@
+KZT = 1.234
+GBP = 23.05
+EUR = 0.21
+ARS = 2.7891
+
 while True:
     try:
-        my_coins = float(input("Please, enter the number of mycoins you have:\n> "))
-        e_rate = float(input("Please, enter the exchange rate:\n> "))
+        MyC = float(input("Please, enter the number mycoins you have:\n> "))
         break
     except ValueError:
         print("\nInvalid input, write only numbers\n")
 
-result = e_rate * my_coins
-print(f'The total amount of dollars: {result}')
+KZT_r = MyC * KZT
+GBP_r = MyC * GBP
+EUR_r = MyC * EUR
+ARS_r = MyC * ARS
+
+print(f'''I will get {round(KZT_r, 2)} KZT from the sale of {MyC} mycoins.
+I will get {round(GBP_r, 2)} GBP from the sale of {MyC} mycoins.
+I will get {round(EUR_r, 2)} EUR from the sale of {MyC} mycoins.
+I will get {round(ARS_r, 2)} ARS from the sale of {MyC} mycoins.''')
