06f842ca62556dc2ec177a630c90dca36baf2b0d
Denis <borbotko.den@mail.ru>
2022-06-11 17:35:23 +0300

    currency-exchange final stage

--- currency_exchange/currency-exchange.py
+++ currency_exchange/currency-exchange.py
@@ -1,21 +1,46 @@
-KZT = 1.234
-GBP = 23.05
-EUR = 0.21
-ARS = 2.7891
+import requests
 
 while True:
     try:
-        MyC = float(input("Please, enter the number mycoins you have:\n> "))
+        ur_code = input("Enter your currency code:\n> ").lower()
+        request = requests.get(f'http://www.floatrates.com/daily/{ur_code}.json').json()
         break
     except ValueError:
-        print("\nInvalid input, write only numbers\n")
+        print("\nNon-existent currency code, try again.\n")
 
-KZT_r = MyC * KZT
-GBP_r = MyC * GBP
-EUR_r = MyC * EUR
-ARS_r = MyC * ARS
+cash = dict()
 
-print(f'''I will get {round(KZT_r, 2)} KZT from the sale of {MyC} mycoins.
-I will get {round(GBP_r, 2)} GBP from the sale of {MyC} mycoins.
-I will get {round(EUR_r, 2)} EUR from the sale of {MyC} mycoins.
-I will get {round(ARS_r, 2)} ARS from the sale of {MyC} mycoins.''')
+if ur_code != 'usd':
+    cash['usd'] = request['usd']['rate']
+if ur_code != 'eur':
+    cash['eur'] = request['eur']['rate']
+
+while True:
+
+    while True:
+        try:
+            desired_code = input('Enter desired currency code or "exit":\n> ').lower()
+            if desired_code not in request.keys() and desired_code != "exit" and desired_code != ur_code:
+                raise ValueError
+            break
+        except ValueError:
+            print("\nNon-existent currency code, try again.\n")
+    if desired_code == "exit":
+        break
+
+    while True:
+        try:
+            money = float(input("Enter the amount of money:\n> "))
+            break
+        except ValueError:
+            print("\nInvalid input, please enter only numbers\n")
+
+    if desired_code == ur_code:
+        print(f"\nHmm.. You received {money}\n")
+    elif desired_code in cash.keys():
+        print("\nChecking the cache...\nIt is in the cache!")
+        print(f'You received {round(money * cash[f"{desired_code}"], 2)}\n')
+    elif desired_code not in cash.keys():
+        print(f'\nChecking the cache...\nSorry, but it is not in the cache!')
+        cash[f'{desired_code}'] = request[f'{desired_code}']['rate']
+        print(f'You received {round(money * cash[f"{desired_code}"], 2)}\n')
