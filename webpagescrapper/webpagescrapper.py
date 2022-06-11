10a32d10df0b835c985c2495cb5e0e361102c1a3
Denis <borbotko.den@mail.ru>
2022-06-11 17:53:03 +0300

    webpagescrapper st2

--- webpagescrapper/webpagescrapper.py
+++ webpagescrapper/webpagescrapper.py
@@ -1,8 +1,22 @@
 import requests
+from bs4 import BeautifulSoup
 
-URL = input("Input the URL:\n> ")
-r = requests.get(f'{URL}')
-if r.status_code != 200:
+url = input("Enter the URl:\n> ")
+r = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
+if 'imdb.com' not in url or 'title' not in url:
+    print("Invalid quote resource!")
+elif r.status_code != 200:
     print("Invalid quote resource!")
 else:
-    print(r.json()["content"])
+    soup = BeautifulSoup(r.content, "lxml")
+
+    title = soup.find('title')
+    split_title = title.text.split(sep=" ")
+    w = 0
+    for item in split_title:
+        if "(" in item:
+            w = split_title.index(item)
+
+    description = soup.find('meta', {'name': 'description'})
+    result = {'title': ' '.join(split_title[0:w]), 'description': description['content']}
+    print(result)
